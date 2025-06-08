f77e0280ff22eb482b6033a45c38f47211b4dd73
Ariel Navas <arielnavas05@gmail.com>
2025-06-08 17:36:33 -0500

    ui pc

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
         <footer className='w-full pb-10 mb-[100px] md:mb-5' id='contact'>
             <div className='flex flex-col items-center'>
                 <h1 className='flex flex-col items-center text-4xl md:text-6xl font-bold bg-gradient-to-b from-gray-100 to-purple-400 bg-clip-text text-transparent
-                lg:max-w-[50vw]'>
+                lg:max-w-[60vw]'>
                     {language === 'EN' ? title : titleES}
                 </h1>
                 <p className='text-white-200 md:mt-10 my-5 text-center'>{language === 'EN' ? description : descriptionES}</p>
